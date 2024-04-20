afcbc5be58c8b33192d78ffd077d902940627367
Prathamesh Dandage <78398209+P-Dandage@users.noreply.github.com>
2024-04-20 08:17:10 +0530

    Delete Cloud-Computing-Projects directory

--- Cloud-Computing-Projects/Problem Statements/hello-world-app/main.py
+++
@@ -1,7 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write("hello world")
-        
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
